fef83e0627b7f8e44dd98050dc79a529743aaaaa
joan <nyongesajoannanjala@gmail.com>
2020-05-17 23:59:08 +0300

    added total price calculation users medium choice

--- js/scripts.js
+++ js/scripts.js
@@ -65,6 +65,20 @@ let price, totalPrice;
                         } else {
                             totalPrice = (price * number) + 280;
                         }
+                        break;
+                        case size = "medium":
+                            price = 600;
+                            if (crust === "thin") {
+                                totalPrice = (price * number) + 100;
+                            } else if (crust === "thick") {
+                                totalPrice = (price * number) + 150;
+                            } else if (crust === "flatbread") {
+                                totalPrice = (price * number) + 180;
+                            } else {
+                                totalPrice = (price * number) + 280;
+                            }
+                            
+    
 
 // // proceed button
 // $(document).ready(function(){
